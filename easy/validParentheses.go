c17eb55cf72f137f890994c30942503eab94757e
ddd <ddk@examl.com>
2020-03-06 12:07:27 +0800

    1.validParentheses V1

--- easy/validParentheses.go
+++ easy/validParentheses.go
@@ -45,11 +45,59 @@ import (
 	"os"
 )
 
+/*
 func main () {
 	//fmt.Print(os.Args[1:])
 	for args := range os.Args {
 		fmt.Println(":", args)
 	}
+
+	return
+}
+*/
+
+func main () {
+	if 2 > len(os.Args) {
+		fmt.Println(true)
+		return
+	}
+
+	var i, j int = 0, 0
+	for i = 1; i < len(os.Args); i++ {
+		fmt.Println(os.Args[i])
+	}
+
+
+	var stack [10] string
+
+	var bracketsmap map[string]string
+	bracketsmap = make(map[string]string)
+	bracketsmap["{"] = "}"
+	bracketsmap["["] = "]"
+	bracketsmap["("] = ")"
+
+	var match bool = true
+	for i = 1; i < len(os.Args); i++ {
+		b, ok := bracketsmap[os.Args[i]]
+		if ok {
+			stack[j] = b
+			j++
+		} else {
+			if stack[j - 1] == os.Args[i] {
+				stack[j - 1] = ""
+				j--
+			} else {
+				match = false
+				break
+			}
+		}
+	}
+
+	if 0 != j {
+		match = false
+	}
+
+	fmt.Println(match)
 
 	return
 }
